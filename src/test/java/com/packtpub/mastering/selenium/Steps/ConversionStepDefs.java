fcb2888b5434732e1ecc7ca4a74690ba55fdd8bf
Simon Wilby <simon.wilby@nextgear.local>
2016-10-28 16:23:31 +0100

    modify step definitions

--- src/test/java/com/packtpub/mastering/selenium/Steps/ConversionStepDefs.java
+++ src/test/java/com/packtpub/mastering/selenium/Steps/ConversionStepDefs.java
@@ -1,32 +1,47 @@
 package com.packtpub.mastering.selenium.Steps;
 
+import com.packtpub.mastering.selenium.pageobjects.Google;
+import com.packtpub.mastering.selenium.pageobjects.TemperatureConverterPage;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 /**
  * Created by simon.wilby on 28/10/2016.
  */
 public class ConversionStepDefs {
-    @Given("^I want to  convert (\\d+)\\.(\\d+) degree Fahrenheit to Celsius$")
-    public void i_want_to_convert_degree_Fahrenheit_to_Celsius(int arg1, int arg2) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        //fail("It is failed");
-        throw new PendingException();
+    private WebDriver driver;
+    public Google googlePage;
+    public TemperatureConverterPage temperatureConverterPage;
+    @Given("^I want to  convert (\\d+).(\\d+) degree Fahrenheit to Celsius$")
+    public void I_want_to_convert_degree_Fahrenheit_to_Celsius(int arg1, int arg2) throws Throwable {
+        System.setProperty("webdriver.chrome.driver", "C:\\Selenium_Training\\Chrome_Driver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        googlePage = new Google(driver);
+        temperatureConverterPage = googlePage.goToTemperatureConversionPage();
     }
 
-    @When("^I input the value of Fahrenheit as (\\d+)\\.(\\d+) in text field$")
-    public void i_input_the_value_of_Fahrenheit_as_in_text_field(int arg1, int arg2) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @When("^I input the value of Fahrenheit as (\\d+).(\\d+) in text field$")
+    public void I_input_the_value_of_Fahrenheit_as_in_text_field(int arg1, int arg2) throws Throwable {
+        //input Fahrenheit value
+        temperatureConverterPage.inputFahrenheit(98.6);
     }
 
     @Then("^It should be converted to Celsius as (\\d+) degree$")
-    public void it_should_be_converted_to_Celsius_as_degree(int arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void It_should_be_converted_to_Celsius_as_degree(int arg1) throws Throwable {
+        //assertion
+        assertEquals(temperatureConverterPage.actualResult(), temperatureConverterPage.expectedResult());
+        driver.quit();
     }
 }
